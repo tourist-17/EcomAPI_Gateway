c5fc3537542993e5c348c43178c98012c9ff2ac2
tourist-17 <yadsak00@gmail.com>
2024-02-25 22:05:59 +0530

    Added rateLimit and middleware of isAuthenticated before placing the  order

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
-const express = require('express');
-
+const express = require("express");
+const morgan = require("morgan");
+const ratelimit = require("express-rate-limit");
+const axios = require("axios");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 
 const PORT = 3004;
+const limiter = ratelimit({
+  windowMs: 2 * 60 * 1000,
+  max: 5,
+});
+
+app.use(morgan("combined"));
+app.use(limiter);
+
+app.use("/orderservice", async (req, res, next) => {
+  //   console.log(req.headers["x-access-token"]);
+
+  try {
+    // console.log(req.query["userid"]);
+    const response = await axios.get(
+      "http://localhost:3003/api/v1/isauthenticated",
+      {
+        headers: {
+          "x-access-token": req.headers["x-access-token"],
+        },
+      }
+    );
+
+    if (response.data) {
+      next();
+    } else {
+      return res.status(401).json({
+        message: "Unauthorised",
+      });
+    }
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorised",
+    });
+  }
+});
+app.use(
+  "/orderservice",
+  createProxyMiddleware({
+    target: "http://localhost:3002/",
+    changeOrigin: true,
+  })
+);
+app.get("/info", (req, res) => {
+  return res.json({ message: "OK" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server Started at port ${PORT}`);
 });
-
